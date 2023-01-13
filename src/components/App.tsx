9cf15df34892069806bd792ff95762bcb957badc
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-13 00:23:59 -0600

    router setup main

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,21 @@
-import { ConnectButton, RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { WagmiConfig } from 'wagmi'
-import { chains, wagmiClient } from 'wallet/config'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { Home } from 'pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { WagmiConfig } from 'wagmi';
+import { chains, wagmiClient } from 'wallet/config';
 
-// async function useBlockNumber(provider: ethers.providers.Provider) {
-//   const x = await provider.getBlockNumber()
-
-//   return x
-// }
-
-const App = () => {
-    // const block = useBlockNumber(provider)
-
+function App() {
     return (
         <WagmiConfig client={wagmiClient}>
             <RainbowKitProvider chains={chains}>
-                <div className="relative overflow-hidden bg-white">
-                    <div className="h-screen p-32">
-                        <ConnectButton />
-                    </div>
-                </div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </BrowserRouter>
             </RainbowKitProvider>
         </WagmiConfig>
-    )
+    );
 }
 
-export default App
+export default App;
