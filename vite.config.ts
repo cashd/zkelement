9fc6cd8f1ef43b670f102b19ebf73165097e2b23
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-13 00:23:09 -0600

    loader wasm stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-})
+    root: './',
+    publicDir: 'public',
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: `${path.dirname(
+                        require.resolve(`@aztec/sdk`)
+                    )}/barretenberg.wasm`,
+                    dest: '',
+                },
+                {
+                    src: `${path.dirname(
+                        require.resolve(`@aztec/sdk`)
+                    )}/web_worker.js`,
+                    dest: '',
+                },
+            ],
+        }),
+    ],
+});
