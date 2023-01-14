f8553c3d7668f71285a3209d00a26fbe6d07c399
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-13 18:31:39 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import '@rainbow-me/rainbowkit/styles.css'
-import App from 'components/App'
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import App from 'components/App.js';
+import { createRoot } from 'react-dom/client';
+import 'tailwindcss/tailwind.css';
 
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
 
-root.render(<App />)
+root.render(<App />);
