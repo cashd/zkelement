96dd6531c1023b36d8a77a6794436ac730847077
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-20 16:25:45 -0600

    fix vite config filetype

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -8,29 +6,20 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // @ RYAN I had to convert all config files to cjs if project is an esm module
 // https://github.com/sveltejs/vite-plugin-svelte/issues/141
 // I dont think this current setup will work with a prod build hmm
-
+// console.log(path.dirname(require.resolve(`@aztec/sdk`)));
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: './',
-    publicDir: 'public',
     plugins: [
         react(),
         tsconfigPaths(),
-        NodeGlobalsPolyfillPlugin({
-            buffer: true,
-        }),
         viteStaticCopy({
             targets: [
                 {
-                    src: `${path.dirname(
-                        require.resolve(`@aztec/sdk`)
-                    )}/barretenberg.wasm`,
+                    src: `bin/barretenberg.wasm`,
                     dest: '',
                 },
                 {
-                    src: `${path.dirname(
-                        require.resolve(`@aztec/sdk`)
-                    )}/web_worker.js`,
+                    src: `bin/web_worker.js`,
                     dest: '',
                 },
             ],
