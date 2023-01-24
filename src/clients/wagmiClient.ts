bb160d9001cb5e31548703df3520772cf0fcfbda
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-24 17:57:09 -0600

    configure custom provider to wagmi client

--- src/clients/wagmiClient.ts
+++ src/clients/wagmiClient.ts
@@ -1,9 +1,17 @@
-import { createClient, configureChains, mainnet } from 'wagmi';
+import { configureChains, createClient } from 'wagmi';
+import { localhost, mainnet } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, provider, webSocketProvider } = configureChains(
-    [mainnet],
-    [publicProvider()]
+const RPC_KEY = import.meta.env.VITE_RPC_KEY;
+
+if (!RPC_KEY) {
+    throw new Error('Provide an RPC_KEY variable in .env');
+}
+
+const { provider, webSocketProvider } = configureChains(
+    [mainnet, localhost],
+    [alchemyProvider({ apiKey: RPC_KEY }), publicProvider()]
 );
 
 export const client = createClient({
