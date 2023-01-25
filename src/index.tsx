1984dc2d1bcaad65ef48d2169852d6eb22d48e1e
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-25 01:49:41 -0600

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import App from 'components/App.js';
 import { createRoot } from 'react-dom/client';
-import 'tailwindcss/tailwind.css';
+import './globals.css';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
