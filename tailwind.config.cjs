ffabd397e1e823dd2e5c54cb4cf12f6011a2f756
Cash DeLeon <cashd@users.noreply.github.com>
2023-01-29 20:59:53 -0600

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,29 +8,30 @@ module.exports = {
             },
         },
         daisyui: {
-            themes: [
-                {
-                    mytheme: {
-                        primary: '#67e8f9',
+            themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+            // themes: [
+            //     {
+            //         mytheme: {
+            //             primary: '#67e8f9',
 
-                        secondary: '#f0abfc',
+            //             secondary: '#f0abfc',
 
-                        accent: '#fde047',
+            //             accent: '#fde047',
 
-                        neutral: '#191D24',
+            //             neutral: '#191D24',
 
-                        'base-100': '#f5f5f4',
+            //             'base-100': '#f5f5f4',
 
-                        info: '#0891b2',
+            //             info: '#0891b2',
 
-                        success: '#36D399',
+            //             success: '#36D399',
 
-                        warning: '#FBBD23',
+            //             warning: '#FBBD23',
 
-                        error: '#F87272',
-                    },
-                },
-            ],
+            //             error: '#F87272',
+            //         },
+            //     },
+            // ],
         },
     },
     plugins: [require('daisyui')],
